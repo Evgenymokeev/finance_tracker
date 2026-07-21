da51c125257e2c5c84dcba9299c444d854ae1f0b
Evgeny Mokeev <mepraha2015@gmail.com>
2026-07-21 20:40:11 +0200

    Add JWT authentication

--- config/settings.py
+++ config/settings.py
@@ -125,6 +125,10 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS":
         "drf_spectacular.openapi.AutoSchema",
 
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
--- config/urls.py
+++ config/urls.py
@@ -6,11 +6,31 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    path("api/expenses/", include("expenses.urls")),
+    path(
+        "api/expenses/",
+        include("expenses.urls"),
+    ),
+
+    path(
+        "api/auth/login/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+
+    path(
+        "api/auth/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
 
     path(
         "api/schema/",
